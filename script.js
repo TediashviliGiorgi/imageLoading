4181fbf30cc61f3c0506c36fe6b0cacff5082138
TediashviliGiorgi <personalmailgt@yahoo.com>
2021-03-31 17:59:10 +0400

    creating js

---
+++ script.js
@@ -0,0 +1,25 @@
+const loadText = document.querySelector('.loading-txt');
+const bg = document.querySelector('.bg');
+
+let load = 0;
+
+let int = setInterval(blurring, 30);
+
+function blurring () {
+    load ++
+    if (load > 99){
+        clearInterval(int)
+    }
+    loadText.innerText = `${load}%`;
+    loadText.style.opacity = 1 
+    loadText.style.opacity = 1 - load /100
+
+    bg.style.filter = `blur(${30- (load / 3.3)}px)`
+    
+     console.log((load / 3.3))
+     
+}
+
+
+
+
